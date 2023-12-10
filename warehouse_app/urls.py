983a868ec65af7e1524fe51756928dd53731c81e
Amr Gharaba <amr_ibr@live.com>
2023-12-10 11:46:02 +0200

    added shipment

--- warehouse_app/urls.py
+++ warehouse_app/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('add_store',views.add_store),
     path('add_item',views.add_item),
     path('store_delete/<id>',views.store_delete),
+    path('logout',views.logout),
 ]
